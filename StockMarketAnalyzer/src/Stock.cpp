c351822d3af948ef333124ca0f2a5d0e47ff8fe9
Sumukhacr <158743843+Sumukhacr@users.noreply.github.com>
2025-04-14 00:29:40 +0530

    Delete StockMarketAnalyzer directory

--- StockMarketAnalyzer/src/Stock.cpp
+++
@@ -1,9 +0,0 @@
-#include "../include/Stock.hpp"
-#include <iostream>
-
-Stock::Stock(std::string symbol, std::string date, double price)
-    : symbol(symbol), date(date), price(price) {}
-
-void Stock::display() const {
-    std::cout << symbol << " | " << date << " | $" << price << std::endl;
-}
